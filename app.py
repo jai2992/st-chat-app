cb67a7c5a7f1378f4ca18519d03a611b6cedc434
JAIGURU <jaiguru.l2022ai-ml@sece.ac.in>
2024-10-01 06:39:00 +0530

    title

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import time
 import numpy as np
 import streamlit as st
 
+st.title("Chat with Llama")
 if "GROQ_API_KEY" not in st.secrets:
     st.error("Please create a secrets.toml file with a GROQ_API_KEY")
 else:

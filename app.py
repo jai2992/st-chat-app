377e395252916efd3bdbdc3fa7fcf4bf1054e6eb
JAIGURU <136327019+jai2992@users.noreply.github.com>
2024-12-20 13:55:58 +0000

    added models

--- app.py
+++ app.py
@@ -13,14 +13,7 @@ else:
 
 client = groq.Client(api_key=GROQ_API_KEY)
 
-groq_models = [
-    "llama-3.1-70b-versatile",
-    "llama-3.1-8b-instant",
-    "llama-3.2-1b-preview",
-    "llama-3.2-3b-preview",
-    "llama-3.2-11b-vision-preview",
-    "mixtral-8x7b-32768"
-]
+groq_models = ['llama-3.1-70b-specdec','distil-whisper-large-v3-en', 'gemma2-9b-it', 'gemma-7b-it (DEPRECATED)', 'llama-3.3-70b-versatile', 'llama-3.1-70b-versatile (DEPRECATED)', 'llama-3.1-8b-instant', 'llama-guard-3-8b', 'llama3-70b-8192', 'llama3-8b-8192', 'mixtral-8x7b-32768', 'whisper-large-v3', 'whisper-large-v3-turbo', 'llama3-groq-70b-8192-tool-use-preview', 'llama3-groq-8b-8192-tool-use-preview', 'llama-3.3-70b-specdec', 'llama-3.2-1b-preview', 'llama-3.2-3b-preview', 'llama-3.2-11b-vision-preview', 'llama-3.2-90b-vision-preview']
 
 selected_model = st.sidebar.selectbox("Select a model", groq_models)
 
